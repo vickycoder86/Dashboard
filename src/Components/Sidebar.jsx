b992f28df6baf7a93b8f52a7be2e15cfbdd278cb
vickycoder86 <vijay.jainm1986@gmail.com>
2024-12-14 16:32:39 +0530

    Orderbook_enabled added on customer

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -6,13 +6,9 @@ import { useNavigate } from "react-router-dom";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { hover } from "@testing-library/user-event/dist/hover";
 
 const SideBar = () => {
-  
   const [ismenuOpen, setIsMenuOpen] = useState(false);
-
-
 
   const open = () => {
     setIsMenuOpen(!ismenuOpen);
@@ -42,8 +38,6 @@ const SideBar = () => {
 
   const userName = localStorage.getItem("user_name");
 
- 
-
   return (
     <>
       <span
@@ -54,58 +48,56 @@ const SideBar = () => {
       </span>
 
       <div
-        className={`sidebar fixed top-0 bottom-0 lg:left-0 left-[-300px] p-2 w-[230px] overflow-y-auto text-center bg-black text-white ${ismenuOpen ? "left-[10px]" : "left-0"}`}
+        className={`sidebar fixed top-0 bottom-0 lg:left-0 left-[-300px] p-2 w-[230px] overflow-y-auto text-center bg-black text-white ${
+          ismenuOpen ? "left-[10px]" : "left-0"
+        }`}
       >
         <div className="p-2.5 mt-1 flex items-center">
           <img src={logo} alt="Logo" />
-          <a href="">
-            <i
-              className="bi bi-x ml-12 cursor-pointer lg:hidden"
-              onClick={open}
-            ></i>
-          </a>
+          <i
+            className="bi bi-x ml-12 cursor-pointer lg:hidden"
+            onClick={open}
+          ></i>
         </div>
         <hr className="my-2 border-white" />
-        
-        <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-stone-500">
+
+        <div
+          className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-stone-500"
+          onClick={viewHome}
+        >
           <i className="bi bi-house-door-fill text-white"></i>
-          <button
-            className="text-[15px] ml-4 text-white"
-            onClick={viewHome}
-          >
-            Home
-          </button>
-        </div>
-        
-        <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-stone-500">
-          <i className="bi bi-people-fill text-white"></i>
-          <button
-            className="text-[15px] ml-4 text-white"
-            onClick={viewCustomerList}
-          >
-            Customers
-          </button>
-        </div>
-        
-        <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-stone-400  ">
-        <i className="bi bi-building-fill text-white"></i>
-          <button
-            className="text-[15px] ml-4"
-            onClick={viewCompainesList}
-            
-          >
-            Companies
-          </button>
+          <span className="text-[15px] ml-4 text-white">Home</span>
         </div>
 
-        <div className="flex text-[18px] gap-16" style={{position: "absolute", bottom: "2%", paddingLeft: "13px"}}>
-        <i className="bi bi-gear text-white" title="Setting"></i>
-        <i
-            className="bi-brightness-high cursor-pointer" title="Change Mode coming soon.." // Toggle theme
+        <div
+          className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-stone-500"
+          onClick={viewCustomerList}
+        >
+          <i className="bi bi-people-fill text-white"></i>
+          <span className="text-[15px] ml-4 text-white">Customers</span>
+        </div>
+
+        <div
+          className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-stone-400"
+          onClick={viewCompainesList}
+        >
+          <i className="bi bi-building-fill text-white"></i>
+          <span className="text-[15px] ml-4">Companies</span>
+        </div>
+
+        <div
+          className="flex text-[18px] gap-16"
+          style={{ position: "absolute", bottom: "2%", paddingLeft: "13px" }}
+        >
+          <i className="bi bi-gear text-white" title="Setting"></i>
+          <i
+            className="bi-brightness-high cursor-pointer"
+            title="Change Mode coming soon.." // Toggle theme
           ></i>
           <i
             className="bi bi-box-arrow-right cursor-pointer text-white"
-            onClick={logout} title="Logout"
+            onClick={logout}
+            title="Logout"
           ></i>
         </div>
 
@@ -125,8 +117,5 @@ const SideBar = () => {
     </>
   );
 };
-
-
-
 
 export default SideBar;
